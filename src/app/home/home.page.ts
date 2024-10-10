b390a8f9b5823dded4277613b12c77c3204fc306
Sofi <sofiiimillan@gmail.com>
2024-10-10 22:03:26 +0200

    done the API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component, OnInit} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardTitle,
+  IonCardSubtitle, IonCardHeader, IonCardContent
+} from '@ionic/angular/standalone';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HttpClientModule, IonCard, IonCardTitle, IonCardSubtitle, IonCardHeader, IonCardContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.fetchDetails();
+  }
+
+  private fetchDetails() {
+    this.http.get('https://fakestoreapi.com/products').subscribe(
+      (response:any) => {
+        console.log(response);
+      }
+    )
+  }
 }
